8795e67c10a3cbe615a6feb1a0398abdefb2fdf0
Ana Grisham <anaunique@icloud.com>
2023-06-21 14:52:36 -0700

    added table data and started server data code

--- db/schema.sql
+++ db/schema.sql
@@ -5,28 +5,28 @@ USE employee_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR (30)
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR (30),
-    salary INT NOT NULL,
-    department INT,
-    FOREIGN KEY (department),
-    REFERENCES department(id),
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department)
+    REFERENCES department(id) 
     ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
-    role INT,
-    FOREIGN KEY (role),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id)
     REFERENCES role(id),
     manager_id INT,
-    FOREIGN KEY (manager_id),
-    REFERENCES employee(id),
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
     ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,25 @@
 INSERT INTO department (name)
 VALUES  ("Sales"),
-        ("Legal")
+        ("Legal"),
         ("Finance"),
         ("Engineering");
 
-INSERT INTO role (title, salary, department)
-VALUES  ("Sales Lead", 100000, "Sales"),
-        ("Salesperson", 80000, "Sales"),
-        ("Lead Engineer", 150000, "Engineering"),
-        ("Software Engineer", 120000, "Engineering"),
-        ("Account Manager", 160000, "Finance"),
-        ("Accountant", 125000, "Finance"),
-        ("Legal Team Lead", 250000, "Legal"),
-        ("Lawyer", 190000, "Legal");
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
+        ("Lead Engineer", 150000, 4),
+        ("Software Engineer", 120000, 4),
+        ("Account Manager", 160000, 3),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 2),
+        ("Lawyer", 190000, 2);
 
-INSERT INTO employee (first_name, last_name, role)
-VALUES
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("John", "Doe", 1, NULL),
+        ("Mike", "Chan", 2, 1),
+        ("Jane", "Doe", 3, NULL),
+        ("Janet", "Jackson", 4, 3),
+        ("Molly", "Green", 5, NULL),
+        ("Morgan", "Freewoman", 6, 5),
+        ("Ben", "Doverman", 7, NULL),
+        ("Phil", "McKracen", 8, 7);
